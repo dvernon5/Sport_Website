e4ce82684f4e3385c2e19cea770cf98a24cd3ffd
Donovan Vernon <d.vernon5@gmail.com>
2026-04-01 12:21:23 -0700

    Added the HTML Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+    return (
+        <header>
+            <nav>
+                <a href="../index.html">
+                    <img src="../src/assets/self-avatar.png" alt="avatar-image"></img>
+                </a>
+                <ul>
+                    <a href="pages/favorite-sport.html"><li>Favorite Sport</li></a>
+                    <a href="pages/favorite-team.html"><li>Favorite Team</li></a>
+                    <a href="pages/favorite-player.html"><li>Favorite Player</li></a>
+                    <a href="pages/contact.html"><li>Contact Me</li></a>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
